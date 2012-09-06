f6ec33f09f56f2f75f3c6bade4c6bedb361d21d8
kepa <kepa@casa-PC.lan>
2012-09-05 23:55:33 -0600

    ProyectoPEP

--- application/controllers/consultor/consultores.php
+++ application/controllers/consultor/consultores.php
@@ -1,17 +1,22 @@
 <?php
+
 /**
  * Contendrá todos los metodos utilizados para definir las pantallas relacionadas
-   a la gestión de los proyectos PEP
+ * a las Consultoras y Consultores.
  *
  * @author Ing. Karen Peñate
  */
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class ProyectoPep extends CI_Controller {
+class Consultores extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+    }
+    
+    public function registraConsultores(){
+        
     }
     
     
---
+++ application/views/componente2/muestraConsultores_view.php
@@ -0,0 +1,29 @@
+    <center>
+    <h1>Gestión de Proyectos PEP</h1>
+</center>
+<table align="center" >
+    <tr>
+        <td align="center" ><a href="<?php echo base_url('componente2/comp23_E1/registrarReunion'); ?>"><img src="<?php echo base_url('resource/imagenes/add.png'); ?>"/></a></td>
+    </tr>
+    <tr>
+        <td class="letraazul" align="center">Registrar Proyecto PEP</td>
+    </tr>
+</table>
+<p></p>
+<table align="center">
+    <thead>
+        <tr>
+            <th class="thEstandar"> Nombre Proyecto</th>
+            <th class="thEstandar">Consultor</th>
+            <th class="thEstandar">Acuerdo Municipal</th>
+            <th class="thEstandar">Información Preliminar</th>
+            <th class="thEstandar">Inventario de Información</th>
+            <th class="thEstandar">Grupo de Apoyo</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+</table>
+
+
