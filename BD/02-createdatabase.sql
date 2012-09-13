172a270ebc97b086c050c9eac18e4cc0d27f9829
kepa <kepa@casa-PC.lan>
2012-09-12 21:06:14 -0600

    Crear Base

---
+++ BD/02-createdatabase.sql
@@ -0,0 +1,11 @@
+﻿-- Database: smpfgl
+
+-- DROP DATABASE smpfgl;
+
+CREATE DATABASE smpfgl
+  WITH OWNER = smpfgl
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'es_SV.UTF-8'
+       LC_CTYPE = 'es_SV.UTF-8'
+       CONNECTION LIMIT = -1;
