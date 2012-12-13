779f0cf8e86be9aa3d9c94dca561e0092dc70b53
Carlos Romero <hotsaucekco@hotmail.com>
2012-12-12 18:27:00 -0600

    actualziacion al 12 del dic del 2012

--- application/views/mapas/partidos_politicos_view.php
+++ application/views/mapas/partidos_politicos_view.php
@@ -1,5 +1,5 @@
 <div style="margin-left: 200px;">
-    <h1><?php echo $tema ?></h1>
+    <h1><?php echo $tema; echo base_url(); ?></h1>
     <div class="cell-page" id="map-area">
         <iframe width="800" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://localhost/cgi-bin/mapserv?map=/var/www/sispfgl/capas/partidos_de_gobierno/partidospormuni.map&layers=all"></iframe>
     </div>
