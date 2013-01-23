acc392016e171a15747bb67b1282ecaa4b53b926
kepa <kepa@casa-PC.lan>
2013-01-22 22:08:10 -0600

    Correcciones, Etapa del 1-4 terminado, Proceso administrativo al 50% y Etapa 0 al 25%

--- application/views/componente2/subcomp23/proceso_administrativo/propuestaTecnica_view.php
+++ application/views/componente2/subcomp23/proceso_administrativo/propuestaTecnica_view.php
@@ -1,6 +1,6 @@
 <script type="text/javascript">        
     $(document).ready(function(){
-        $("#cancelar").button().click(function() {
+        $("#cancelar").button().hide().click(function() {
             document.location.href='<?php echo base_url(); ?>';
         });
         
@@ -22,27 +22,32 @@
         });
         
         $('#selMun').change(function(){
+            $('#propuestaTecnicaForm')[0].reset();
+            $("#cancelar").hide();
+            $("#guardar").hide();
             $.getJSON('<?php echo base_url('componente2/procesoAdministrativo/cargarPropuestaTecnica') . "/" ?>'+$('#selMun').val(), 
             function(data) {
                 var i=0;
                 $.each(data, function(key, val) {
                     if(key=='rows'){
                         $.each(val, function(id, registro){
-                            $('#consultoresInteres').setGridParam({
-                                url:'<?php echo base_url('componente2/procesoAdministrativo/cargarConsultoraInteres2') . '/' ?>'+registro['cell'][0],
-                                datatype:'json'
-                            }).trigger('reloadGrid');
                             $('#pro_id').val(registro['cell'][0]);
                             $('#pro_numero').val(registro['cell'][1]);
-                            $('#pro_finicio').val(registro['cell'][2]);
-                            $('#pro_ffinalizacion').val(registro['cell'][3]);
+                            $('#pro_fsolicitud').val(registro['cell'][2]);
+                            $('#pro_frecepcion').val(registro['cell'][3]);
+                            $('#pro_faperturatecnica').val(registro['cell'][4]);
+                            $('#pro_faperturafinanciera').val(registro['cell'][5]);
+                            $('#pro_fcierre_negociacion').val(registro['cell'][6]);
+                            $('#pro_ffirma_contrato').val(registro['cell'][7]);
+                            $("#cancelar").show();
+                            $("#guardar").show();
                         });                    
                     }
                 });
             });              
         });
         
-        $( "#pro_finicio" ).datepicker({
+        $("#pro_fsolicitud" ).datepicker({
             showOn: 'both',
             buttonImage: '<?php echo site_url('resource/imagenes/calendario.png'); ?>',
             buttonImageOnly: true, 
@@ -52,17 +57,41 @@
         /*FIN DEL DATEPICKER*/
         
         /*PARA EL DATEPICKER*/
-        $( "#pro_ffinalizacion" ).datepicker({
+        $( "#pro_frecepcion" ).datepicker({
             showOn: 'both',
             buttonImage: '<?php echo site_url('resource/imagenes/calendario.png'); ?>',
             buttonImageOnly: true, 
             dateFormat: 'dd/mm/yy'
         });
         
+        $( "#pro_faperturatecnica" ).datepicker({
+            showOn: 'both',
+            buttonImage: '<?php echo site_url('resource/imagenes/calendario.png'); ?>',
+            buttonImageOnly: true, 
+            dateFormat: 'dd/mm/yy'
+        });
+        $( "#pro_faperturafinanciera" ).datepicker({
+            showOn: 'both',
+            buttonImage: '<?php echo site_url('resource/imagenes/calendario.png'); ?>',
+            buttonImageOnly: true, 
+            dateFormat: 'dd/mm/yy'
+        });
+        $( "#pro_fcierre_negociacion" ).datepicker({
+            showOn: 'both',
+            buttonImage: '<?php echo site_url('resource/imagenes/calendario.png'); ?>',
+            buttonImageOnly: true, 
+            dateFormat: 'dd/mm/yy'
+        });
+        $( "#pro_ffirma_contrato" ).datepicker({
+            showOn: 'both',
+            buttonImage: '<?php echo site_url('resource/imagenes/calendario.png'); ?>',
+            buttonImageOnly: true, 
+            dateFormat: 'dd/mm/yy'
+        });
         /*FIN DEL DATEPICKER*/
         
         /*ZONA DE BOTONES*/
-        $("#guardar").button().click(function() {
+        $("#guardar").button().hide().click(function() {
             this.form.action='<?php echo base_url('componente2/procesoAdministrativo/guardarProceso') . "/2" ?>';
         });
                
@@ -77,112 +106,81 @@
                 }
             }
         });
-        /*FIN ZONA VALIDACIONES*/
-     
-        /*VARIABLES*/
-        var tabla=$("#consultoresInteres");
-        tabla.jqGrid({
-            url:'<?php echo base_url('componente2/procesoAdministrativo/cargarConsultoraInteres2') . '/0' ?>',
-            editurl:'<?php echo base_url('componente2/procesoAdministrativo/gestionarConsultoresInteres') . '/0' ?>',
-            datatype:'json',
-            altRows:true,
-            height: "100%",
-            hidegrid: false,
-            colNames:['id','Nombre','Tipo','Aplica',''],
-            colModel:[
-                {name:'con_int_id',index:'con_int_id', width:40,editable:false,editoptions:{size:15} },
-                {name:'con_int_nombre',index:'con_int_nombre',width:200,editable:false,
-                    editoptions:{size:25,maxlength:50}, 
-                    formoptions:{label: "Nombre:",elmprefix:"(*)"},
-                    editrules:{required:true} 
-                },
-                {name:'con_int_tipo',index:'con_int_tipo',editable:false,edittype:"select",width:60,
-                    editoptions:{ value: '0:Seleccione;Empresa:Empresa;ONG:ONG' }, 
-                    formoptions:{ label: "Tipo:",elmprefix:"(*)"}
-                },
-                {name:'con_int_aplica',index:'con_int_aplica',width:60,align:'center',editable:true,
-                    edittype:"checkbox",editoptions:{value:"Si:No"}},
-                {name:'actions',formatter:"actions",editable:false,fixed:true,width:60,
-                    formatoptions:{"keys":true,delbutton: false}
-                }
-            ],
-            multiselect: false,
-            caption: "Consultoras que han manifestado interés",
-            rowNum:10,
-            rowList:[10,20,30],
-            loadonce:true,
-            pager: jQuery('#pagerConsultoresInteres'),
-            viewrecords: true
-        }).jqGrid('navGrid','#pagerConsultoresInteres',
-        {edit:false,add:false,del:false,search:false,refresh:false,
-            beforeRefresh: function() {
-                tabla.jqGrid('setGridParam',{datatype:'json',loadonce:true}).trigger('reloadGrid');}
-        }
-    ).hideCol('con_int_id');
-        /* Funcion para regargar los JQGRID luego de agregar y editar*/
-        function despuesAgregarEditar() {
-            tabla.jqGrid('setGridParam',{datatype:'json',loadonce:true}).trigger('reloadGrid');
-            return[true,'']; //no error
-        }
-                
+                      
     });
 </script>
 
 
-<form id="SeleccionConsultoraForm" method="post">
-    <center>
-        <h2 class="h2Titulos">Pedido de propuesta técnica y financiera</h2>
-        <br/>
-        <table>
-            <tr>
-            <td><strong>Departamento</strong></td>
-            <td><select id='selDepto'>
-                    <option value='0'>--Seleccione--</option>
-                    <?php foreach ($departamentos as $depto) { ?>
-                        <option value='<?php echo $depto->dep_id; ?>'><?php echo $depto->dep_nombre; ?></option>
-                    <?php } ?>
-                </select>
-            </td>
-            </tr>
-            <td><strong>Municipio</strong></td>
-            <td><select id='selMun' name='selMun'>
-                    <option value='0'>--Seleccione--</option>
-                </select>
-            </td>
-            </tr>
-        </table>
-    </center>
-    <br/><br/><br/>
-    <table class="procesoAdmin">
+
+<center>
+    <h2 class="h2Titulos">Pedido de propuesta técnica y financiera</h2>
+    <br/>
+    <table>
+        <tr>
+        <td><strong>Departamento</strong></td>
+        <td><select id='selDepto'>
+                <option value='0'>--Seleccione--</option>
+                <?php foreach ($departamentos as $depto) { ?>
+                    <option value='<?php echo $depto->dep_id; ?>'><?php echo $depto->dep_nombre; ?></option>
+                <?php } ?>
+            </select>
+        </td>
+        </tr>
+        <td><strong>Municipio</strong></td>
+        <td><select id='selMun' name='selMun'>
+                <option value='0'>--Seleccione--</option>
+            </select>
+        </td>
+        </tr>
+    </table>
+</center>
+<br/><br/><br/>
+<form id="propuestaTecnicaForm" method="post">
+    <table class="procesoAdmin" border="0" cellspacing="0" >
         <tr>
         <td class="textD"><strong>No. Proceso: </strong></td>
         <td> <input value="" id="pro_numero" name="pro_numero" type="text" size="10" readonly="readonly" style="border: none; background: white;"/></td>
         </tr>
         <tr>
-        <td class="textD"><strong>Fecha de inicio: </strong></td> 
-        <td><input value="" id="pro_finicio" name="pro_finicio" type="text" size="10" readonly="readonly"/></td>
+        <td class="textD"><strong>Fecha de solicitud: </strong></td> 
+        <td><input value="" id="pro_fsolicitud" name="pro_fsolicitud" type="text" size="10" readonly="readonly"/></td>
         </tr>
         <tr>
-        <td class="textD"> <strong>Fecha de finalización: </strong></td>
-        <td><input value="" id="pro_ffinalizacion" name="pro_ffinalizacion" type="text" size="10" readonly="readonly"/></td>
+        <td class="textD"> <strong>Fecha de recepción: </strong></td>
+        <td><input value="" id="pro_frecepcion" name="pro_frecepcion" type="text" size="10" readonly="readonly"/></td>
+        </tr>
+        <tr>
+        <td colspan="2" class="fechaAdmin" >Fecha de apertura</td>
+        </tr>
+        <tr>
+        <td class="textD fechaAdmin"> <strong>Técnica: </strong></td>
+        <td class="fechaAdmin"><input value="" id="pro_faperturatecnica" name="pro_faperturatecnica" type="text" size="10" readonly="readonly"/></td>
+        </tr>
+        <tr>
+        <td class="textD fechaAdmin"> <strong>Financiera: </strong></td>
+        <td class="fechaAdmin"><input value="" id="pro_faperturafinanciera" name="pro_faperturafinanciera" type="text" size="10" readonly="readonly"/></td>
+        </tr>
+        <tr>
+        <td colspan="2" class="fechaAdmin" ></td>
+        </tr>
+        <tr>
+        <td class="textD"> <strong>Fecha de cierre de negociación: </strong></td>
+        <td><input value="" id="pro_fcierre_negociacion" name="pro_fcierre_negociacion" type="text" size="10" readonly="readonly"/></td>
+        </tr>
+        <tr>
+        <td class="textD"> <strong>Fecha de firma de contrato: </strong></td>
+        <td><input value="" id="pro_ffirma_contrato" name="pro_ffirma_contrato" type="text" size="10" readonly="readonly"/></td>
         </tr>
     </table>
     <br/><br/>
-    <center>
-        <table id="consultoresInteres"></table>
-        <div id="pagerConsultoresInteres"></div>
-        <br/>
-    </center>
+
     <center style="position: relative;top: 20px">
         <input type="submit" id="guardar" value="Guardar" />
         <input type="button" id="cancelar" value="Cancelar" />
     </center>
-    <input id="pro_id" name="pro_id" value="" style="visibility: hidden"/>
+    <input id="pro_id" name="pro_id" value="" style="visibility: hidden"/>    
 </form>
 
 <div id="mensaje" class="mensaje" title="Aviso de la operación">
     <p>La acción fue realizada con satisfacción</p>
-</div>
-<div id="mensaje2" class="mensaje" title="Aviso">
-    <p>Debe Seleccionar una fila para continuar</p>
 </div>
