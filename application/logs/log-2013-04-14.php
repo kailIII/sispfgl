8fc8f67c76148b8dca3d38bea3c7737f07a50a98
kofykep <cfuentes_86@hotmail.com>
2013-04-14 12:45:29 -0600

    Arreglando .gitignore

---
+++ application/logs/log-2013-04-14.php
@@ -0,0 +1,81 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-04-14 12:32:57 --> Severity: Warning  --> pg_pconnect(): Unable to connect to PostgreSQL server: FATAL:  la autentificaci?n password fall? para el usuario &lt;&lt;sispfgl&gt;&gt;
+FATAL:  la autentificaci?n password fall? para el usuario &lt;&lt;sispfgl&gt;&gt; /var/www/sispfgl/system/database/drivers/postgre/postgre_driver.php 99
+ERROR - 2013-04-14 12:32:57 --> Unable to connect to the database
+ERROR - 2013-04-14 12:32:57 --> Could not find the language line "db_error_heading"
+ERROR - 2013-04-14 12:32:57 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-14 12:32:57 --> 404 Page Not Found --> favicon.ico
+ERROR - 2013-04-14 12:33:31 --> Severity: Warning  --> pg_pconnect(): Unable to connect to PostgreSQL server: could not connect to server: Connection refused
+	Is the server running on host &quot;192.168.1.4&quot; and accepting
+	TCP/IP connections on port 5432? /var/www/sispfgl/system/database/drivers/postgre/postgre_driver.php 99
+ERROR - 2013-04-14 12:33:31 --> Unable to connect to the database
+ERROR - 2013-04-14 12:33:31 --> Could not find the language line "db_error_heading"
+ERROR - 2013-04-14 12:33:32 --> Severity: Warning  --> pg_pconnect(): Unable to connect to PostgreSQL server: could not connect to server: Connection refused
+	Is the server running on host &quot;192.168.1.4&quot; and accepting
+	TCP/IP connections on port 5432? /var/www/sispfgl/system/database/drivers/postgre/postgre_driver.php 99
+ERROR - 2013-04-14 12:33:32 --> Unable to connect to the database
+ERROR - 2013-04-14 12:33:32 --> Could not find the language line "db_error_heading"
+ERROR - 2013-04-14 12:34:17 --> Severity: Warning  --> pg_pconnect(): Unable to connect to PostgreSQL server: could not connect to server: Connection refused
+	Is the server running on host &quot;192.168.1.4&quot; and accepting
+	TCP/IP connections on port 5432? /var/www/sispfgl/system/database/drivers/postgre/postgre_driver.php 99
+ERROR - 2013-04-14 12:34:17 --> Unable to connect to the database
+ERROR - 2013-04-14 12:34:17 --> Could not find the language line "db_error_heading"
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:38:17 --> 404 Page Not Found --> sispfgl
+ERROR - 2013-04-14 12:41:36 --> Severity: Warning  --> Missing argument 3 for Comp23_E1::subirArchivo() /var/www/sispfgl/application/controllers/componente2/comp23_E1.php 719
+ERROR - 2013-04-14 12:41:36 --> Severity: Notice  --> Undefined variable: campo /var/www/sispfgl/application/controllers/componente2/comp23_E1.php 720
+ERROR - 2013-04-14 12:41:36 --> Severity: Warning  --> pg_query(): Query failed: ERROR:  error de sintaxis en o cerca de «=»
+LINE 1: ...a_archivo,'/.0') ruta_archivo FROM seguimiento WHERE =seg_id
+                                                                ^ /var/www/sispfgl/system/database/drivers/postgre/postgre_driver.php 176
+ERROR - 2013-04-14 12:41:36 --> Query error: ERROR:  error de sintaxis en o cerca de «=»
+LINE 1: ...a_archivo,'/.0') ruta_archivo FROM seguimiento WHERE =seg_id
+                                                                ^
+ERROR - 2013-04-14 12:41:36 --> Could not find the language line "db_error_heading"
