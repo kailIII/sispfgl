a739dd66cb87277c46dcb392819fe37e82f42cbc
Karen Peñate <karen.elvira.penate@gmail.com>
2013-11-13 05:09:42 -0600

    Reporte de la planificacion Anual Operativa Componente 1

--- application/views/poa/seguimiento/programacion_anio_view.php
+++ application/views/poa/seguimiento/programacion_anio_view.php
@@ -3,6 +3,16 @@
         $('#boton').button().click(function() {
             document.location.href = '<?php echo base_url($ruta . 'gestionProgramacionAnual'); ?>/' +$('#anio').val()+"/"+ $('#poa_com_id').val();
         });
+        
+        $('#reporte').button().click(function() {
+            $.ajax({
+                type: "POST",
+                success: function(response)
+                {
+                    window.open(response);
+                }
+            });
+        });
     });
 </script>
 
@@ -11,19 +21,28 @@
     <h2 class="h2Titulos">Planificación Operativa Anual</h2>
     <h2 class="h2Titulos">Planificación Por Año</h2>
     <br/>
-    <select id="poa_com_id">
-        <option selected="selected" value="0">Seleccione--</option>
-        <?php foreach ($componentes as $aux) { ?>
-            <option value='<?php echo $aux->poa_com_id; ?>'><?php echo $aux->poa_com_codigo . ' ' . $aux->poa_com_descripcion; ?></option>
-        <?php } ?>
-    </select>
-    <br/> <br/>
-    <select id="anio">
-        <option selected="selected" value="<?php echo date('Y'); ?>"><?php echo date('Y'); ?></option>
-        <option value="<?php echo date('Y') + 1; ?>"><?php echo date('Y') + 1; ?></option>
-    </select>
-    <br/><br/>
-    <input id='boton' type="button"  value="Editar Programación Anual" />
+    <form id="formulario" method="post" action="<?php echo base_url('reportes/reportesPOA'); ?>">
+        <select id="poa_com_id">
+            <option selected="selected" value="0">Seleccione--</option>
+            <?php foreach ($componentes as $aux) { ?>
+                <option value='<?php echo $aux->poa_com_id; ?>'><?php echo $aux->poa_com_codigo . ' ' . $aux->poa_com_descripcion; ?></option>
+            <?php } ?>
+        </select>
+        <br/> <br/>
+        <select id="anio" name="anio">
+            <option selected="selected" value="<?php echo date('Y'); ?>"><?php echo date('Y'); ?></option>
+            <option value="<?php echo date('Y') + 1; ?>"><?php echo date('Y') + 1; ?></option>
+        </select>
+        <br/><br/>
+
+        <input id='boton' type="button"  value="Editar Programación Anual" />
+
+        <br/><br/>
+        <input type="submit" id="reporte" value="Generar Reporte" />
+
+    </form>
+
+
 </center>
 
 
